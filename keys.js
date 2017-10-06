4149212822233af92471a830bd9a28131e32721d
Jack Tamas <lovely_gad@yahoo.com>
2017-10-05 17:59:58 -0700

    my final submit

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.twitterKeys = {
+  consumer_key: 'b6QfEQYQMaU0wVOZBMvsAjeuQ',
+  consumer_secret: 'gCsrn84BPYaBMXpekZ8x4dFRtiizsB3F4jOVxPXewfRyMaGBKX',
+  access_token_key: '363241487-pPYZdQlst0CDnF1702NshIzViHMBW0LSvXnIiAGt',
+  access_token_secret: '0fVoZfwq4ESjtjuEqvgwqTSwhXSTM2Yfjjhy7oCDO1Eix'
+}
+exports.spotifyKeys = {
+  id: '662e82cf2e9745f1996a5e48fe74068d',
+  secret: '7a1a4a5d9ffa42c59b5cf4a4d7a4899f'
+}
+
+exports.omdbKey = '40e9cece';
---
+++ liri.js
@@ -0,0 +1,135 @@
+
+var spotify = require('node-spotify-api');
+var twitter = require('twitter');
+var keys = require('./keys.js');
+var fs = require('fs');
+var request = require('request');
+var stringify = require("json-stringify-pretty-compact");
+var randomText = fs.readFileSync('./random.txt', 'utf8').split(',');
+var randomCommand = randomText[0];
+var randomQuery = randomText[1];
+var args = process.argv.slice(2);
+var command = args[0];
+var apiQuery = process.argv.slice(3).join(' ');
+
+function displayInfo(){
+  if(command == null){
+    console.log('Please use the following commands:');
+    console.log('To search for tweets use the command (my-tweets) and type a tweet word after.');
+    console.log('To search for a song use the command (spotify-this-song) and type a song name after.');
+    console.log('To search for a movie use the command (movie-this) and type type a movie name after.');
+    console.log('To search for something random use the command (do-what-it-says).');
+  }
+}
+displayInfo();
+
+switch (command) {
+  case "my-tweets":
+  twitterApi(apiQuery);
+  break;
+
+  case "spotify-this-song":
+  spotifyApi(apiQuery);
+  break;
+
+  case "movie-this":
+  omdbApi(apiQuery);
+  break;
+
+  case "do-what-it-says":
+  doWhatItSays(randomCommand, randomQuery);
+  break;
+}
+
+function twitterApi(query){
+  console.log('Loading.......');
+  if(query == ''){
+    query = 'San Diego meetup';
+  }
+  var client = new twitter(keys.twitterKeys);
+  client.get('search/tweets', {q: query, count: 20}, function(error, tweets, response) {
+    if(error){
+      console.log(error);
+    }
+    if(tweets.statuses.length == 0){
+      console.log('Please use another word')
+    }
+    for (var i=0; i < tweets.statuses.length; i++){
+      console.log('Tweets about ==> ',query )
+      console.log('Created at:', tweets.statuses[i].created_at)
+      console.log(tweets.statuses[i].text)
+      console.log('------------------------------------------------------------'+"\n");
+    }
+    console.log('TOTAL TWEETS:', tweets.statuses.length);
+  });
+
+}
+
+function spotifyApi(query){
+  console.log('Loading.......');
+  var spoti = new spotify(keys.spotifyKeys);
+  if(query == ''){
+    query = 'the sign Ace of Base';
+  }
+  spoti.search({ type: 'track', query: query, limit: 5 }, function(err, data) {
+    if (data.tracks.items.length == 0){
+      console.log('Plaease try another song or check your spelling')
+    }
+    if (err) {
+      console.log(err);
+    }
+    for (var i=0; i < data.tracks.items.length; i++){
+      console.log('Artist:',data.tracks.items[i].artists[0].name);
+      console.log('Song:',data.tracks.items[i].name);
+      console.log('Album:',data.tracks.items[i].album.name);
+      console.log('Link to the Song:',data.tracks.items[i].external_urls.spotify);
+      console.log('-------------------------------------------------------------');
+    }
+  });
+}
+
+function omdbApi(query){
+  console.log('Loading.......');
+  if(query == ''){
+    query = 'Mr. Nobody';
+  }
+  var queryUrl = 'http://www.omdbapi.com/?apikey=40e9cece&t='+query;
+  request(queryUrl, function(err, response, body){
+    if(JSON.parse(response.body).Error){
+      console.log('Sorry', JSON.parse(response.body).Error)
+      console.log('Plaease try another movie or check your spelling')
+    }
+    else if(err){
+      console.log('Error:', err);
+    }
+    else if(JSON.parse(body).hasOwnProperty('Title')) {
+      console.log('Title:', JSON.parse(body).Title);
+      console.log('Year:',JSON.parse(body).Year);
+      console.log('IMDB Rating:',JSON.parse(body).Ratings[0].Value);
+      //Checking if the array of Ratings has the Rotten Tomatoes Value
+      if (JSON.parse(body).Ratings[1]){
+        console.log('Rotten Tomatoes Rating:',JSON.parse(body).Ratings[1].Value);
+      }
+      console.log('Conutry:',JSON.parse(body).Country);
+      console.log('Language:',JSON.parse(body).Language);
+      console.log('Plot:',JSON.parse(body).Plot);
+      console.log('Actors:',JSON.parse(body).Actors);
+      console.log('Image:',JSON.parse(body).Poster);
+    }
+  });
+}
+function doWhatItSays(command, query){
+  switch (command) {
+    case "my-tweets":
+    twitterApi(query);
+    break;
+
+    case "spotify-this-song":
+    spotifyApi(query);
+    break;
+
+    case "movie-this":
+    omdbApi(query);
+    break;
+  }
+}
