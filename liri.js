450c25657274dd285efa986e9d937d50ecbf8184
Jack Tamas <lovely_gad@yahoo.com>
2017-10-05 19:09:09 -0700

    final app

--- liri.js
+++ liri.js
@@ -1,19 +1,26 @@
-
+//Created all needed variables and packages
 var spotify = require('node-spotify-api');
 var twitter = require('twitter');
 var keys = require('./keys.js');
 var fs = require('fs');
 var request = require('request');
 var stringify = require("json-stringify-pretty-compact");
+
+//reading the text file and convert it to an array
 var randomText = fs.readFileSync('./random.txt', 'utf8').split(',');
+//getting the random command from the array
 var randomCommand = randomText[0];
+//getting the random query from the array
 var randomQuery = randomText[1];
+//getting the args from the shell
 var args = process.argv.slice(2);
 var command = args[0];
+//getting the query from the process.argv array and convert it to a string
 var apiQuery = process.argv.slice(3).join(' ');
 
-function displayInfo(){
-  if(command == null){
+//Checking if nothing was entered give the user some information how the app works
+function displayLiriGuide(){
+  if(command == null || !isNaN(command)){
     console.log('Please use the following commands:');
     console.log('To search for tweets use the command (my-tweets) and type a tweet word after.');
     console.log('To search for a song use the command (spotify-this-song) and type a song name after.');
@@ -21,8 +28,9 @@ function displayInfo(){
     console.log('To search for something random use the command (do-what-it-says).');
   }
 }
-displayInfo();
+displayLiriGuide();
 
+//Managing which api to call depending on the user command
 switch (command) {
   case "my-tweets":
   twitterApi(apiQuery);
@@ -41,10 +49,11 @@ switch (command) {
   break;
 }
 
+
 function twitterApi(query){
   console.log('Loading.......');
   if(query == ''){
-    query = 'San Diego meetup';
+    query = 'San Diego weather';
   }
   var client = new twitter(keys.twitterKeys);
   client.get('search/tweets', {q: query, count: 20}, function(error, tweets, response) {
@@ -52,7 +61,7 @@ function twitterApi(query){
       console.log(error);
     }
     if(tweets.statuses.length == 0){
-      console.log('Please use another word')
+      console.log('Please use another word');
     }
     for (var i=0; i < tweets.statuses.length; i++){
       console.log('Tweets about ==> ',query )
@@ -73,7 +82,7 @@ function spotifyApi(query){
   }
   spoti.search({ type: 'track', query: query, limit: 5 }, function(err, data) {
     if (data.tracks.items.length == 0){
-      console.log('Plaease try another song or check your spelling')
+      console.log('Plaease try another song or check your spelling');
     }
     if (err) {
       console.log(err);
@@ -96,8 +105,7 @@ function omdbApi(query){
   var queryUrl = 'http://www.omdbapi.com/?apikey=40e9cece&t='+query;
   request(queryUrl, function(err, response, body){
     if(JSON.parse(response.body).Error){
-      console.log('Sorry', JSON.parse(response.body).Error)
-      console.log('Plaease try another movie or check your spelling')
+      console.log('Sorry', JSON.parse(response.body).Error,'Plaease try another movie or check your spelling');
     }
     else if(err){
       console.log('Error:', err);
@@ -105,11 +113,16 @@ function omdbApi(query){
     else if(JSON.parse(body).hasOwnProperty('Title')) {
       console.log('Title:', JSON.parse(body).Title);
       console.log('Year:',JSON.parse(body).Year);
-      console.log('IMDB Rating:',JSON.parse(body).Ratings[0].Value);
+
+      //Checking if the array of Ratings has the IMDB Rating Value
+      if (JSON.parse(body).Ratings[0]){
+        console.log('IMDB Rating:',JSON.parse(body).Ratings[0].Value);
+      }
       //Checking if the array of Ratings has the Rotten Tomatoes Value
       if (JSON.parse(body).Ratings[1]){
         console.log('Rotten Tomatoes Rating:',JSON.parse(body).Ratings[1].Value);
       }
+
       console.log('Conutry:',JSON.parse(body).Country);
       console.log('Language:',JSON.parse(body).Language);
       console.log('Plot:',JSON.parse(body).Plot);
